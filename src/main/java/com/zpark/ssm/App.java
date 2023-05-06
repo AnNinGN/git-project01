27930a4f3358b9ed96ae2d42c2179d58bac85a38
anning <anning@163.com>
2023-05-06 10:12:14 +0800

    an master first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -6,5 +6,8 @@ public class App {
         System.out.println("Hello Git！");
         System.out.println("Hello Git！anning master second commit!");
         System.out.println("Hello Git！anning hotfix first commit!");
+        System.out.println("Hello Git！anning hotfix first modify!");
+        System.out.println("Hello Git！master hotfix first modify!");
+
     }
 }
