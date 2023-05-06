018ef1e55881a25400f29a64033e4bba52ef0af6
anning <anning@163.com>
2023-05-06 09:06:39 +0800

    an hotfix first commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args){
         System.out.println("Hello Git！");
-        System.out.println("Hello Anning！");
+        System.out.println("Hello Git！anning master second commit!");
+        System.out.println("Hello Git！anning hotfix first commit!");
     }
 }
